d5eddd25127374d1ab2e304ef7bd472e18c2d0e3
Erol Kaftanoglu <erolkaftanoglu@gmail.com>
2020-01-05 14:34:48 +0300

    Update

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@ git add .
 git commit -m "Update"
 git push develop develop
 
+rm -rf public
+
 hugo -D
 
 cd public
+echo 'erolkaftanoglu.com' >> CNAME  
 git add .
 git commit -m "webSite"
 git push origin master

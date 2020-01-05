00bd530e18efd15fd242150122bc7fb53e022109
Erol Kaftanoglu <erolkaftanoglu@gmail.com>
2020-01-05 14:35:26 +0300

    Update

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ hugo -D
 
 cd public
 echo 'erolkaftanoglu.com' >> CNAME  
+git init 
+git remote add origin https://github.com/erolkaftanoglu/erolkaftanoglu.github.io.git
 git add .
 git commit -m "webSite"
 git push origin master
